9047dbd34298e7c653b9602e239646175a5564fd
Alvin Dizon <alvin.dizon91@gmail.com>
2020-06-29 14:34:44 +0800

    My solution for exercise 3
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetector.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetector.java
@@ -8,11 +8,11 @@ public class IntervalsAdjacencyDetector {
      * @return true if the intervals are adjacent, but don't overlap
      */
     public boolean isAdjacent(Interval interval1, Interval interval2) {
-        // this implementation contains two bugs:
-        // 1. will erroneously report adjacent if interval1 and interval2 are the same
-        // 2. will erroneously report adjacent if interval1 after interval2
-        return interval1.getEnd() == interval2.getStart() || interval1.getStart() >= interval2.getEnd()
-                || isSameIntervals(interval1, interval2);
+        // check for the following
+        // 1. check if ends of intervals are the same. this will take care of overlaps
+        // 2. check if intervals are the same.
+        return interval1.getEnd() == interval2.getStart() || interval1.getStart() == interval2.getEnd()
+                && !isSameIntervals(interval1, interval2);
     }
 
     private boolean isSameIntervals(Interval interval1, Interval interval2) {
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,53 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class IntervalsAdjacencyDetectorTest {
 
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+    // interval 1 is adjacent to interval 2
+    @Test
+    public void isAdjacent_interval1IsAdjacentToInterval2_trueReturned() throws Exception {
+        boolean result = SUT.isAdjacent(new Interval(-1, 0), new Interval(0, 2));
+        assertThat(result, is(true));
+    }
+    // interval 1 is before interval 2
+    @Test
+    public void isAdjacent_interval1IsBeforeInterval2_falseReturned() throws Exception {
+        boolean result = SUT.isAdjacent(new Interval(-2, 2), new Interval(3, 4));
+        assertThat(result, is(false));
+    }
+
+    // interval 1 is after interval 2
+    @Test
+    public void isAdjacent_interval1IsAfterInterval2_falseReturned() throws Exception {
+        boolean result = SUT.isAdjacent(new Interval(-2, 2), new Interval(-4, -3));
+        assertThat(result, is(false));
+    }
+
+    // same intervals
+    @Test
+    public void isAdjacent_interval1IsEqualToInterval2_falseReturned() throws Exception {
+        boolean result = SUT.isAdjacent(new Interval(-2, 2), new Interval(-2, 2));
+        assertThat(result, is(false));
+    }
+
+    // interval 1 is after and adjacent to interval 2
+    @Test
+    public void isAdjacent_interval1IsAfterAndAdjacentToInterval2_falseReturned() throws Exception {
+        boolean result = SUT.isAdjacent(new Interval(0, 10), new Interval(10, 12));
+        assertThat(result, is(false));
+    }
 }

164576710105c88e77978c5fff93293c7d7bf3c9
Alvin Dizon <alvin.dizon91@gmail.com>
2020-06-29 09:16:25 +0800

    Solution to exercise 1
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,37 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void negativeTest() {
+        boolean result = SUT.isNegative(-1);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void positiveTest() {
+        boolean result = SUT.isNegative(4);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void zeroTest() {
+        boolean result = SUT.isNegative(0);
+        Assert.assertThat(result, is(false));
+    }
+
 
 }
